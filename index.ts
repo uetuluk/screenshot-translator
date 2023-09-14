0424f0c49ed5916e78afeea149c7a22905d45585
Utku Ege Tuluk <36725861+uetuluk@users.noreply.github.com>
2023-09-14 15:44:33 +0800

    feat: working example

--- index.ts
+++ index.ts
@@ -1,14 +1,70 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import multer from 'multer';
+import { createWorker } from 'tesseract.js';
+import Anthropic from '@anthropic-ai/sdk';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+const storage = multer.memoryStorage(); // Store the image in memory
+const upload = multer({ storage: storage });
+
+async function initTesseractWorker() {
+  const worker = await createWorker({
+    logger: progress => console.log(progress)
+  });
+
+  await worker.loadLanguage('jpn');
+  await worker.initialize('jpn');
+  return worker;
+}
+
+const worker = await initTesseractWorker();
+
+const anthropic = new Anthropic();
+
+app.post('/upload', upload.single('screenshot'), async (req, res) => {
+  if (!req.file || !req.file.buffer) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const image: Buffer = req.file.buffer;
+
+  worker.recognize(image).then(({ data: { text } }) => {
+    res.send(text);
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send(error);
+  });
+  
 });
+
+app.use(bodyParser.json());
+
+app.post('/translate', async (req, res) => {
+  if (!req.body.text) {
+    return res.status(400).send('No text provided.');
+  }
+  console.log("Translating: " + req.body.text);
+  await anthropic.completions.create({
+    model: 'claude-2',
+    max_tokens_to_sample: 1000,
+    prompt: `${Anthropic.HUMAN_PROMPT} Translate the given text to English:\n${req.body.text}${Anthropic.AI_PROMPT}`,
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send(error);
+  }).then((completion) => {
+    console.log(completion);
+    res.send(completion?.completion);
+  });
+});
+
+app.use(express.static('public'));
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ public/app.js
@@ -0,0 +1,82 @@
+function previewImage(file) {
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            document.getElementById('imagePreview').src = e.target.result;
+        }
+
+        reader.readAsDataURL(file);
+        reader.onloadend = () => uploadFile(file); // Upload file after previewing
+    }
+}
+
+document.getElementById('screenshot').addEventListener('change', function() {
+    previewImage(this.files[0]);
+});
+
+const dropZone = document.getElementById('dropZone');
+dropZone.addEventListener('dragover', function(e) {
+    e.preventDefault();
+    this.style.backgroundColor = '#eaeaea';  // change background color to give visual feedback
+});
+
+dropZone.addEventListener('dragleave', function(e) {
+    this.style.backgroundColor = '';  // reset background color
+});
+
+dropZone.addEventListener('drop', function(e) {
+    e.preventDefault();
+    this.style.backgroundColor = '';  // reset background color
+
+    const file = e.dataTransfer.files[0];
+    document.getElementById('screenshot').files = e.dataTransfer.files;  // set the dropped file to the file input
+    previewImage(file);
+});
+
+
+function uploadFile(file) {
+    const formData = new FormData();
+    formData.append('screenshot', file);
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('extracted_test').value = text;
+        document.getElementById('extractedTextPreview').textContent = text;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('extractedTextPreview').textContent = 'Error uploading or processing the image.';
+    });
+}
+
+document.getElementById('translateForm').addEventListener('submit', function(e) {
+    e.preventDefault();  // Prevent the default form submit behavior
+    
+    const textToTranslate = document.getElementById('extracted_test').value;
+
+    fetch('/translate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: textToTranslate })
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        if (data) {
+            document.getElementById('extractedTextPreview').textContent = data;
+        } else {
+            throw new Error("Translation not received");
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('extractedTextPreview').textContent = 'Error translating the text.';
+    });
+});

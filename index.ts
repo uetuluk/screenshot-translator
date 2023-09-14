92111a22af562793652a18dfddc9db98949aa693
Utku Ege Tuluk <36725861+uetuluk@users.noreply.github.com>
2023-09-14 15:52:49 +0800

    fix: ocr settings

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import multer from 'multer';
-import { createWorker } from 'tesseract.js';
+import { createWorker, OEM } from 'tesseract.js';
 import Anthropic from '@anthropic-ai/sdk';
 import bodyParser from 'body-parser';
 
@@ -18,8 +18,8 @@ async function initTesseractWorker() {
     logger: progress => console.log(progress)
   });
 
-  await worker.loadLanguage('jpn');
-  await worker.initialize('jpn');
+  await worker.loadLanguage('jpn+eng');
+  await worker.initialize('jpn+eng', OEM.LSTM_ONLY);
   return worker;
 }
 
